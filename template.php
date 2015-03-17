5f0b4bf7a65f0378f0f24253b412691c552db32f
gabesullice <gabriel@sullice.com>
2015-03-17 09:16:39 -0600

    Add template.php comments and entry point.

--- template.php
+++ template.php
@@ -2,6 +2,19 @@
 
 define('PUMPJACK_DEBUG', TRUE);
 
+/**
+ * Implements hook_page_alter().
+ */
+function pumpjack_page_alter($vars) {
+  pumpjack_add_js();
+}
+
+/**
+ * Central function for adding javascript assets.
+ *
+ * Here, we have no conditions, but this is where we would conditionally
+ * add our files.
+ */
 function pumpjack_add_js() {
   $actions = array(
     'bxslider',
@@ -17,8 +30,12 @@ function pumpjack_add_js() {
   ));
 }
 
+/**
+ * Helper function to add actions and plugins.
+ */
 function _pumpjack_add_js($action, $plugin = FALSE) {
   $pumpjack_root = &drupal_static(__FUNCTION__ . 'root', drupal_get_path('theme', 'pumpjack'));
+  // Load minified js when debugging is off.
   $suffix = (PUMPJACK_DEBUG) ? '.js' : '.min.js';
 
   if ($plugin) drupal_add_js($pumpjack_root . '/js/plugins/jquery.' . $action . $suffix);
